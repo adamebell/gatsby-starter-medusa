279a6940ead2eebd15fe7872520b28a21638a452
Adam Bell <43587308+adamebell@users.noreply.github.com>
2022-02-13 13:53:50 -0500

    removing countries from region dropdown

--- src/components/header/region-popover.jsx
+++ src/components/header/region-popover.jsx
@@ -15,8 +15,8 @@ const RegionPopover = () => {
 
   const disabled = useRegionDisabled()
 
-  const handleSelect = async (region, country) => {
-    updateRegion(region, country.display_name)
+  const handleSelect = async (region) => {
+    updateRegion(region)
   }
 
   return (
@@ -36,21 +36,20 @@ const RegionPopover = () => {
       <PopoverTransition>
         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 px-6 py-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           {regions?.map(region => {
-            return region.countries.map((country, i) => {
               return (
                 <div className="border-b border-ui last:border-b-0" key={i}>
                   <Menu.Item>
                     {() => (
                       <button
-                        onClick={() => handleSelect(region, country)}
+                        onClick={() => handleSelect(region)}
                         className={classNames(
-                          country === country.display_name
+                          region === region.name
                             ? "text-black"
                             : "text-ui-dark",
                           "block py-3 text-sm w-full text-left hover:text-black"
                         )}
                       >
-                        {country.display_name} /{" "}
+                        {region.name} /{" "}
                         {region.currency_code.toUpperCase()}
                       </button>
                     )}
