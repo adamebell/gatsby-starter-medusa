2dc26e88b45413e2a6ef0cd73627629950ab34aa
Adam Bell <43587308+adamebell@users.noreply.github.com>
2022-02-13 14:52:20 -0500

    Update region-popover.jsx

--- src/components/header/region-popover.jsx
+++ src/components/header/region-popover.jsx
@@ -28,14 +28,11 @@ const RegionPopover = () => {
     >
       <div>
         <Menu.Button className="inline-flex justify-center w-full px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:text-gray-600">
-          Region Select {region?.currency_code?.toUpperCase()}
+          Region: {region.name} {region?.currency_code?.toUpperCase()}
         </Menu.Button>
       </div>
-
       <PopoverTransition>
         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 px-6 py-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-
-
           {regions?.map((region, i) => {
             return (
                 <div className="border-b border-ui last:border-b-0" key={i}>
@@ -58,7 +55,7 @@ const RegionPopover = () => {
                 </div>
               )
             })
-          })}
+          }
         </Menu.Items>
       </PopoverTransition>
     </Menu>
